533fa13256446ed429a8192417b76c03a77025bd
Hao Shi <boruoshihao@gmail.com>
2019-11-15 18:20:58 -0500

    test different version.

---
+++ v0.0.2/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#","page":"-","title":"-","text":"#AFQMC.jl Documentation","category":"page"},{"location":"#","page":"-","title":"-","text":"greet()","category":"page"},{"location":"#AFQMC.greet-Tuple{}","page":"-","title":"AFQMC.greet","text":"greet()\n\nPrint \"Hellow World!\"\n\n\n\n\n\n","category":"method"}]
+}
